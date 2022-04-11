64482c8eb35dbf332e11fe8cca827998fe0f3e25
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-04-11 18:53:03 +0600

    asd

--- src/Сomponents/atoms/serviceList/Service.jsx
+++ src/Сomponents/atoms/serviceList/Service.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import styles from "./styles.module.scss"
 
-const Service = ({title,text,beforeImage}) => {
-  return (
-    <div className={styles.wrapper} >
-     <div>
-        <img src={beforeImage} alt="beforeImage" />
-     </div>
-     <div>
-        <h2>{title}</h2>
-        <p>{text}</p>
-      </div>
-       
-    </div>
-  )
+const Service = ({title, text, beforeImage}) => {
+    return (
+        <div className={styles.wrapper}>
+            <div>
+                <img src={beforeImage} alt="beforeImage"/>
+            </div>
+            <div>
+                <h2 className={styles.title}>{title}</h2>
+                <p className={styles.subtitle}>{text}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Service
